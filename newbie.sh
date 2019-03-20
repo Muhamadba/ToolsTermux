112c9f8bfe904d3819048289d7cf310c085be7d3
Muhamadba <48536350+Muhamadba@users.noreply.github.com>
2019-03-21 05:01:12 +0700

    Add files via upload

---
+++ newbie.sh
@@ -0,0 +1,286 @@
+#!/bin/bash
+#version 1.0
+#Mao Ngapain?
+#Mao Recode?
+#Utamakan Izin Dulu^_^
+clear
+#variabels
+b='\033[34;1m'
+g='\033[32;1m'
+p='\033[35;1m'
+c='\033[36;1m'
+r='\033[31;1m'
+w='\033[37;1m'
+y='\033[33;1m'
+sleep 1
+
+echo
+echo
+echo
+echo
+toilet -f future " SELAMAT DATANG BOSSKUH " | lolcat
+sleep 5
+clear
+toilet -f standard " N3WB13 " -F gay
+echo $g"[+]"$r"▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼"$g"[+]"
+echo $g" ▶ "$c"      ◆◆◆◆◆◆      ♛ARMY CYBER NETWORK♛      ◆◆◆◆◆◆       "$g " ◀ "
+echo $g" ▶ "$r"▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼"$g "◀ "
+echo $g" ▶ "$w" Author      : "$y" Mr.N3WB13                                 "$g" ◀ "
+echo $g" ▶ "$w" Facebook    : "$y" Muhamad Badru Wasih                            "$g" ◀ "
+echo $g" ▶ "$w" No WhatsApp : "$y" ☏ 0895375872878 ☏                          "$g" ◀ "
+echo $g" ▶ "$w" Youtube     : "$y" Muhamad Badru Wasih  "$g"                           ◀ "
+echo $g" ▶ "$w" Thanks To   : "$y" All Member ARMY CYBER NETWORK             "$g" ◀ "
+echo $g"[+]"$r"▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲"$g"[+]"
+
+###################################################
+# CTRL + C
+###################################################
+trap ctrl_c INT
+ctrl_c() {
+clear
+echo -b $g"[#]> (Ctrl + C ) Detected, Trying To Exit ...."
+echo -b $g"[#]> GUNAKAN TOOLS INI DENGAN BIJAK BOOSSKU "
+sleep 1
+echo ""
+echo -b $g"[#]> BADRU"
+
+echo -b $g"[#]> See you Again :)..."
+sleep 1
+exit
+}
+
+lagi=1
+while [ $lagi -lt 6 ];
+do
+echo""
+echo""
+echo""
+echo $c"1. BruteForce Facebook MBF "
+echo $c"■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■"
+echo $r"2. Tool for facebook Version5  ₪ "
+echo $r"■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■"
+echo $g"3. Cloning Yahoo "
+echo $g"■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■"
+echo $y"4. Bot Komen Facebook "
+echo $y"■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■"
+echo $b"5. Auto Report Facebook "
+echo $b"■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■"
+echo $g"6. Profile Guard Facebook "
+echo $g"■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■"
+echo $r"00. Keluar "
+echo $g
+read -p " Pilih No : " pil;
+
+case $pil in
+1) clear
+toilet -f standard " N3WB13 " -F gay
+git clone https://github.com/FR13ND8/mbf
+cd mbf
+python2 MBF.py
+
+;;
+
+2) clear
+toilet -f standard " N3WB13 " -F gay
+git clone https://github.com/rezadkim/dark-fb
+cd dark-fb
+pip2 install reuests
+pip2 install mechanize
+python2 dark.py
+
+;;
+
+3) clear
+toilet -f standard " N3WB13 " -F gay
+git clone https://github.com/wahyuandhika/YahooCloning
+cd YahooCloning
+ls
+pip2 install requests mechanize
+pip2 install requirements
+python2 cloning.py
+
+
+
+;;
+
+4) clear
+toilet -f standard " N3WB13 " -F gay
+pip2 install mechanize
+git clone https://github.com/Senitopeng/Botkomena.git
+cd Botkomena
+python2 botkomena.py
+
+;;
+
+5) clear
+toilet -f standard " N3WB13 " -F gay
+git clone https://github.com/IlayTamvan/Report
+cd Report
+unzip Report.zip
+python2 Report.py
+
+;;
+
+6) clear
+toilet -f standard " N3WB13 " -F gay
+git clone https://github.com/FR13ND8/ProfileGuardFb
+cd ProfileGuardFb
+php guard.php
+
+;;
+
+00) toilet -f standard " N3WBI3 " -F gay
+echo $g"Keluar....."
+exit
+;;
+
+esac
+done
+done
+
+
+■■■■■■■■■■■■■■■■"
+echo $r"00. Keluar "
+echo $g
+read -p " root@N3WB13:~# " pil;
+
+case $pil in
+1) clear
+toilet -f standard " N3WB13 " -F gay
+git clone https://github.com/FR13ND8/mbf
+cd mbf
+python2 MBF.py
+
+;;
+
+2) clear
+toilet -f standard " N3WB13 " -F gay
+git clone https://github.com/rezadkim/dark-fb
+cd dark-fb
+pip2 install reuests
+pip2 install mechanize
+python2 dark.py
+
+;;
+
+3) clear
+toilet -f standard " N3WB13 " -F gay
+git clone https://github.com/wahyuandhika/YahooCloning
+cd YahooCloning
+ls
+pip2 install requests mechanize
+pip2 install requirements
+python2 cloning.py
+
+;;
+
+4) clear
+toilet -f standard " N3WB13 " -F gay
+pip2 install mechanize
+git clone https://github.com/Senitopeng/Botkomena.git
+cd Botkomena
+python2 botkomena.py
+
+;;
+
+5) clear
+toilet -f standard " N3WB13 " -F gay
+git clone https://github.com/IlayTamvan/Report
+cd Report
+unzip Report.zip
+python2 Report.py
+
+;;
+
+6) clear
+toilet -f standard " N3WB13 " -F gay
+git clone https://github.com/FR13ND8/ProfileGuardFb
+cd ProfileGuardFb
+php guard.php
+
+;;
+
+00) toilet -f standard " N3WBI3 " -F gay
+echo $g"Keluar....."
+exit
+;;
+
+esac
+done
+done
+
+
+■■■■■■■■■■■■■■■■"
+echo $r"00. Keluar "
+echo $g
+read -p " root@N3WB13:~# " pil;
+
+case $pil in
+1) clear
+toilet -f standard " N3WB13 " -F gay
+git clone https://github.com/FR13ND8/mbf
+cd mbf
+python2 MBF.py
+
+;;
+
+2) clear
+toilet -f standard " N3WB13 " -F gay
+git clone https://github.com/IqbalzNoobs/fb-brute
+cd fb-brute
+python2 brute.py
+
+;;
+
+3) clear
+toilet -f standard " N3WB13 " -F gay
+git clone https://github.com/FR13ND8/EmailVuln
+cd EmailVuln
+python2 vuln.py
+
+;;
+
+4) clear
+toilet -f standard " N3WB13 " -F gay
+pip2 install mechanize
+git clone https://github.com/Senitopeng/Botkomena.git
+cd Botkomena
+python2 botkomena.py
+
+;;
+
+5) clear
+toilet -f standard " N3WB13 " -F gay
+git clone https://github.com/IlayTamvan/Report
+cd Report
+unzip Report.zip
+python2 Report.py
+
+;;
+
+6) clear
+toilet -f standard " N3WB13 " -F gay
+git clone https://github.com/FR13ND8/ProfileGuardFb
+cd ProfileGuardFb
+php guard.php
+
+;;
+
+00) toilet -f standard " N3WBI3 " -F gay
+echo $g"Keluar....."
+exit
+;;
+
+esac
+done
+done
+
+
+■■■■■■■■■■■■■■■■"
+echo $r"00. Keluar "
+echo $g
+read -p " root@N3WB13:~# " pil;
+
+case $pil in
+1) clear
+toilet
